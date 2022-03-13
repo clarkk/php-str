e09bc2be96bbe8d2a78a286d35298043c5a5f749
clarkk <clark@electrobeat.dk>
2022-03-13 23:03:25 +0100

    added format num

--- Format.php
+++ Format.php
@@ -10,7 +10,7 @@ class Format {
 		return json_decode($json, true);
 	}
 	
-	static public function str_amount(float $num, int $dec=0, string $thousand_sep='', string $decimal_sep=''): string{
+	static public function num(float $num, int $dec=0, string $thousand_sep='', string $decimal_sep=''): string{
 		$locale = \dbdata\Lang::get_locale();
 		
 		return number_format($num, $dec, $decimal_sep ?: $locale['decimal_point'], $thousand_sep ?: $locale['thousands_sep']);
@@ -25,5 +25,24 @@ class Format {
 		}
 		
 		return (int)round(((float)str_replace(',', '.', $amount) * 100));*/
+	}
+	
+	static public function datasize(int $int): string{
+		if(!$int){
+			return '0';
+		}
+		
+		foreach([
+			'Mb'	=> 1024 * 1024,
+			'Kb'	=> 1024,
+			'Bytes'	=> 1
+		] as $unit => $value){
+			$scale = $int / $value;
+			if($scale >= 1){
+				return self::num($scale, is_int($scale) ? 0 : 2).' '.$unit;
+			}
+		}
+		
+		return '0';
 	}
 }
