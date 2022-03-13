e399edf869b71750a0783eccb3bde3d788565c68
clarkk <clark@electrobeat.dk>
2022-03-13 21:51:03 +0100

    added str_amount method

--- Format.php
+++ Format.php
@@ -9,4 +9,21 @@ class Format {
 		
 		return json_decode($json, true);
 	}
+	
+	static public function str_amount(float $num, int $dec=0, string $thousand_sep='', string $decimal_sep=''): string{
+		$locale = \dbdata\Lang::get_locale();
+		
+		return number_format($num, $dec, $decimal_sep ?: $locale['decimal_point'], $thousand_sep ?: $locale['thousands_sep']);
+	}
+	
+	static public function amount(string $amount): int{
+		/*$amount = str_replace(' ', '', $amount);
+		$pos_dot = strpos($amount, '.');
+		$pos_comma = strpos($amount, ',');
+		if($pos_dot !== false && $pos_comma !== false){
+			$amount = str_replace($pos_dot > $pos_comma ? ',' : '.', '', $amount);
+		}
+		
+		return (int)round(((float)str_replace(',', '.', $amount) * 100));*/
+	}
 }
