0ca0937de110070e33c916e3119cd5a62037b969
clarkk <clark@electrobeat.dk>
2023-04-05 09:33:55 +0200

    improved normalize

--- Str.php
+++ Str.php
@@ -9,38 +9,29 @@ class Str {
 	const ENC_LATIN 		= 'ISO-8859-1';
 	const ENC_LATIN_WIN 	= 'WINDOWS-1252';
 	
-	//	Non-breaking spaces: https://www.compart.com/en/unicode/U+00A0
+	//	Non-breaking space: https://www.compart.com/en/unicode/U+00A0
 	private const NBSP 				= '\xA0';
 	private const NBSP_UTF8 		= '\xC2'.self::NBSP;
 	private const NBSP_UTF16 		= '\x00'.self::NBSP;
+	private const NBSP_UNICODE 		= '00A0';
 	
 	//	Zero-width space: https://www.compart.com/en/unicode/U+200B
-	private const ZWSP 				= '\x20\x0B'; // same in UTF8
-	//private const ZWNJ 			= '\x20\x0C'; // same in UTF8
-	//private const ZWJ 			= '\x20\x0D'; // same in UTF8
+	private const ZWSP_UTF8 		= '\xE2\x80\x8B';
+	private const ZWSP_UTF16 		= '\x20\x0B';
+	private const ZWSP_UNICODE 		= '200B';
+	//private const ZWNJ 			= '\x20\x0C';
+	//private const ZWJ 			= '\x20\x0D';
 	
 	//	Soft hyphen: https://www.compart.com/en/unicode/U+00AD
 	private const SHY 				= '\xAD';
 	private const SHY_UTF8 			= '\xC2'.self::SHY;
 	private const SHY_UTF16 		= '\x00'.self::SHY;
+	private const SHY_UNICODE 		= '00AD';
 	
-	//	Non-breaking hyphen
-	private const NBHY 				= '\x20\x11'; // same in UTF8
-	
-	/*
-	//	Non-breaking spaces
-	const NBSP 				= "\xA0";
-	const NBSP_UTF8 		= "\xC2".self::NBSP;
-	const NBSP_UNICODE 		= "\x00".self::NBSP;
-	
-	//	Soft hyphen
-	const SHY 				= "\xAD";
-	const SHY_UTF8 			= "\xC2".self::SHY;
-	const SHY_UNICODE 		= "\x00".self::SHY;
-	
-	//	Non-breaking hyphen
-	const NBHY 				= "\x20\x11";
-	*/
+	//	Non-breaking hyphen: https://www.compart.com/en/unicode/U+2011
+	private const NBHY_UTF8 		= '\xE2\x80\x91';
+	private const NBHY_UTF16 		= '\x20\x11';
+	private const NBHY_UNICODE 		= '2011';
 	
 	/*
 		https://www.regular-expressions.info/unicode.html
@@ -53,12 +44,13 @@ class Str {
 	*/
 	
 	private const UNICODE_FILTER_PRINT 		= '\P{Cc}\p{Cf}\p{Cn}\p{Cs}';
+	private const WHITESPACES 				= '\n\r\t';
 	
 	private const PATTERN_FILTER_PRINT 		= '/[^'.self::UNICODE_FILTER_PRINT.']/';
 	private const PATTERN_FILTER_PRINT_N 	= '/[^'.self::UNICODE_FILTER_PRINT.'\n]/';
-	private const PATTERN_FILTER_PRINT_S 	= '/[^'.self::UNICODE_FILTER_PRINT.'\s]/';
+	private const PATTERN_FILTER_PRINT_S 	= '/[^'.self::UNICODE_FILTER_PRINT.self::WHITESPACES.']/';
 	
-	private const PATTERN_MATCH_PRINT_S 	= '/[\PC\s]/';
+	private const PATTERN_MATCH_PRINT_S 	= '/[\PC'.self::WHITESPACES.']/';
 	
 	static public function filter_utf8(string $str, string $encoding=self::ENC_LATIN, string $allow_whitespace='n', bool $strip_mb4=true): string{
 		switch($allow_whitespace){
@@ -93,17 +85,20 @@ class Str {
 		return preg_match(self::PATTERN_FILTER_PRINT_S.'u', $str) ? false : true;
 	}
 	
-	static public function check_printable_ratio(string $str, bool $utf8=false): ?array{
+	static public function check_printable_ratio(string &$str, bool $utf8=false): ?array{
+		self::normalize($str, $utf8);
+		
 		$strlen 		= $utf8 ? mb_strlen($str) : strlen($str);
 		$non_printable 	= preg_match_all(self::PATTERN_FILTER_PRINT_S.($utf8 ? 'u' : ''), $str);
 		$printable 		= preg_match_all(self::PATTERN_MATCH_PRINT_S.($utf8 ? 'u' : ''), $str);
 		
-		if(!$printable || $printable != $strlen - $non_printable){
+		$diff = $printable - $strlen + $non_printable;
+		if(!$printable || $diff){
 			return [
 				'all'			=> $strlen,
 				'non_printable'	=> $non_printable,
 				'printable'		=> $printable,
-				'diff'			=> abs($printable - $strlen + $non_printable)
+				'diff'			=> abs($diff)
 			];
 		}
 		
@@ -133,35 +128,17 @@ class Str {
 		return $str;
 	}
 	
-	static public function normalize(string &$str): void{
-		$str = preg_replace('/'.self::NBSP.'|'.self::ZWSP.'/u', ' ', $str);
-		$str = preg_replace('/'.self::SHY.'|'.self::NBHY.'/u', '-', $str);
-		//$str = preg_replace('/'.self::ZWNJ.'|'.self::ZWJ.'/u', '', $str);
-		
-		/*if(strpos($str, self::NBSP) !== false){
-			$str = strtr($str, [
-				self::NBSP_UTF8 	=> ' ',
-				self::NBSP_UNICODE 	=> ' '
-			]);
-			
-			//	Avoid breaking UTF8 unicode chars
-			$str = preg_replace('/\xA0/u', ' ', $str);
+	static public function normalize(string &$str, bool $utf8=true): void{
+		if($utf8){
+			$str = preg_replace('/\x{'.self::NBSP_UNICODE.'}/u', ' ', $str);
+			$str = preg_replace('/[\x{'.self::ZWSP_UNICODE.'}\x{'.self::SHY_UNICODE.'}]/u', '', $str);
+			$str = preg_replace('/\x{'.self::NBHY_UNICODE.'}/u', '-', $str);
 		}
-		
-		if(strpos($str, self::SHY) !== false){
-			$str = strtr($str, [
-				self::SHY_UTF8 		=> '-',
-				self::SHY_UNICODE 	=> '-'
-			]);
-			
-			//	Avoid breaking UTF8 unicode chars
-			$str = preg_replace('/\xAD/u', '-', $str);
+		else{
+			$str = preg_replace('/'.self::NBSP_UTF16.'|'.self::NBSP_UTF8.'|'.self::NBSP.'/', ' ', $str);
+			$str = preg_replace('/'.self::ZWSP_UTF16.'|'.self::ZWSP_UTF8.'|'.self::SHY_UTF16.'|'.self::SHY_UTF8.'|'.self::SHY.'/', '', $str);
+			$str = preg_replace('/'.self::NBHY_UTF16.'|'.self::NBHY_UTF8.'/', '-', $str);
 		}
-		
-		if(strpos($str, self::NBHY) !== false){
-			//	Avoid breaking UTF8 unicode chars
-			$str = preg_replace('/\x20\x11/u', '-', $str);
-		}*/
 	}
 	
 	static private function strip_mb4(string $str): string{
@@ -172,6 +149,6 @@ class Str {
 		$planes_4_15 	= '[\xF1-\xF3][\x80-\xBF]{3}';
 		$plane_16 		= '\xF4[\x80-\x8F][\x80-\xBF]{2}';
 		
-		return preg_replace("/(?:$planes_1_3|$planes_4_15|$plane_16)/", '', $str);
+		return preg_replace("/$planes_1_3|$planes_4_15|$plane_16/", '', $str);
 	}
 }
